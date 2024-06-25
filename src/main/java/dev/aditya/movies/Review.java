6f3252734be2ee3fd5a40289cccdafd6c747c853
adityachaudhary16 <adityachaudhary1609@gmail.com>
2024-06-24 21:03:05 -0700

    Review class

---
+++ src/main/java/dev/aditya/movies/Review.java
@@ -0,0 +1,18 @@
+package dev.aditya.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "reviews")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data // getter, setter
+public class Review {
+    @Id
+    private ObjectId id;
+    private String body;
+}
