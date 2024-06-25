c0a80f6865bcfe717e67d412fd4966feb51b4556
adityachaudhary16 <adityachaudhary1609@gmail.com>
2024-06-24 22:37:03 -0700

    get movie by id

--- src/main/java/dev/aditya/movies/MovieService.java
+++ src/main/java/dev/aditya/movies/MovieService.java
@@ -1,9 +1,12 @@
 package dev.aditya.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -11,5 +14,9 @@ public class MovieService {
     private MovieRepository movieRepository;
     public List<Movie> allMovies() {
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(ObjectId id) {
+        return movieRepository.findById(id);
     }
 }
